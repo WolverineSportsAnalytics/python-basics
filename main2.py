cf051d47900a51bdb07145b65e3b805ab5107710
Anthony Remick <anthonyremick@0587330712.wireless.umich.net>
2021-10-12 20:51:28 -0400

    Creating a Branch

--- main2.py
+++ main2.py
@@ -2,7 +2,13 @@ def oddOrEven(nums):
     '''Given an unsorted list of numbers, return a list that indicates if the value at each index is odd (0) or even (1).'''
     # EXAMPLE:
     # Given [2, 4, 5, 7, 8, 10], return [1, 1, 0, 0, 1, 1]
-    return []
+    x = []
+    for i in nums:
+        if i % 2 == 0:
+            x.append(1)
+        else:
+            x.append(0)
+    return x
 
 
 def mostOccurences(nums):
@@ -10,7 +16,13 @@ def mostOccurences(nums):
     # Hint: use oddOrEven to test function faster
     # Hint: use a map
     # Hint: https://stackoverflow.com/questions/13098638/how-to-iterate-over-the-elements-of-a-map-in-python
-    return -1 
+    d = {}
+    for i in nums:
+        if i in d.keys():
+          d[i] += 1
+        else:
+            d[i] = 0
+    return max(d, key=d.get)
 
 
 def main():
