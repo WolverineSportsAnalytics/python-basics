4cc57dc3a3b868afe00a46bb4ea98aeabf1326da
casperg <casperg@umich.edu>
2021-10-16 10:20:28 -0400

    Python task #2

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ def oddOrEven(nums):
     '''Given an unsorted list of numbers, return a list that indicates if the value at each index is odd (0) or even (1).'''
     # EXAMPLE:
     # Given [2, 4, 5, 7, 8, 10], return [1, 1, 0, 0, 1, 1]
-    return []
+    return [int(i % 2 == 0) for i in nums]
 
 
 def mostOccurences(nums):
@@ -10,7 +10,7 @@ def mostOccurences(nums):
     # Hint: use oddOrEven to test function faster
     # Hint: use a map
     # Hint: https://stackoverflow.com/questions/13098638/how-to-iterate-over-the-elements-of-a-map-in-python
-    return -1 
+    return max(nums, key=nums.count)
 
 
 def main():
@@ -21,6 +21,12 @@ def main():
     print("\nTesting mostOccurences")
     print("EXPECTED:", 1, "\nACTUAL:", mostOccurences(oddOrEven([2, 4, 5, 7, 8, 10])))
     # Add any additional test cases if needed
+    print("\noddOrEven Test 2\n")
+    print("EXPECTED:", [1, 0, 0, 1, 1, 0], "\n")
+    print("ACTUAL:", oddOrEven([8, 7, 11, 100, -2, 3]), "\n")
+    print("mostOcurrences Test 2\n")
+    print("EXPECTED: 4\n")
+    print("ACTUAL:", mostOccurences([1, 4, 2, 2, 4, 3, 3, 3, 4, 4]))
 
 if __name__ == "__main__":
     main()
