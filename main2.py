a6fef619d0edf71c850b2cce52ec4f334a2145ad
jskulnik <jskulnik@umich.edu>
2021-10-26 19:56:00 -0400

    main2 work

--- main2.py
+++ main2.py
@@ -2,7 +2,13 @@ def oddOrEven(nums):
     '''Given an unsorted list of numbers, return a list that indicates if the value at each index is odd (0) or even (1).'''
     # EXAMPLE:
     # Given [2, 4, 5, 7, 8, 10], return [1, 1, 0, 0, 1, 1]
-    return []
+    list1 = []
+    for x in nums:
+            if x%2 == 0:
+                list1 += [1]
+            else: 
+                list1 += [0]
+    return list1
 
 
 def mostOccurences(nums):
@@ -10,7 +16,25 @@ def mostOccurences(nums):
     # Hint: use oddOrEven to test function faster
     # Hint: use a map
     # Hint: https://stackoverflow.com/questions/13098638/how-to-iterate-over-the-elements-of-a-map-in-python
-    return -1 
+    uniqueCount = {}
+
+    for num in nums:
+        # Check if key already exists, if so, just add one to count
+        if num in uniqueCount:
+            uniqueCount[num] += 1
+        # Key doesn't exist, set count to 1
+        else:
+            uniqueCount[num] = 1
+
+    answer = 0     # answer to keep track of the key
+    maxCount = 0    # maxCount to keep track of max count we've seen in uniqueCount
+    for k in uniqueCount:
+        if uniqueCount[k] > maxCount:
+            answer = k                    # update answer
+            maxCount = uniqueCount[k]     # update maxCount
+
+    return answer
+
 
 
 def main():
