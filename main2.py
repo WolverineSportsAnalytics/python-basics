472459d12515ac00dc743fe45385a819e81c087b
weipeng <weipeng@umich.edu>
2021-10-26 19:42:20 -0400

    finished challenge #2

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ def oddOrEven(nums):
     '''Given an unsorted list of numbers, return a list that indicates if the value at each index is odd (0) or even (1).'''
     # EXAMPLE:
     # Given [2, 4, 5, 7, 8, 10], return [1, 1, 0, 0, 1, 1]
-    return []
+    return [(i + 1) % 2 for i in nums]
 
 
 def mostOccurences(nums):
@@ -10,7 +10,18 @@ def mostOccurences(nums):
     # Hint: use oddOrEven to test function faster
     # Hint: use a map
     # Hint: https://stackoverflow.com/questions/13098638/how-to-iterate-over-the-elements-of-a-map-in-python
-    return -1 
+    mp = {}
+    for k in nums:
+        if k in mp:
+            mp[k] += 1
+        else:
+            mp[k] = 1
+    max, ans = -1, 0
+    for i in mp:
+        if max < mp[i]:
+            max = mp[i]
+            ans = i
+    return ans
 
 
 def main():
