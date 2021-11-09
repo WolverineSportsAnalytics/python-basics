d888b80545361fbf19eb3c93e6aa48b33bb52880
kvtravis <kvtravis@umich.edu>
2021-11-09 00:51:48 -0500

    final for practice 2

--- main2.py
+++ main2.py
@@ -2,15 +2,35 @@ def oddOrEven(nums):
     '''Given an unsorted list of numbers, return a list that indicates if the value at each index is odd (0) or even (1).'''
     # EXAMPLE:
     # Given [2, 4, 5, 7, 8, 10], return [1, 1, 0, 0, 1, 1]
-    return []
+    solution = []
+    for x in nums:
+        if x % 2 == 0:
+            solution.append(1)
+        else:
+            solution.append(0)
+    return solution
 
 
 def mostOccurences(nums):
+    mapone = {}
+
+    for x in nums:
+        if x in mapone:
+            mapone[x] += 1
+        else:
+            mapone[x] = 1
+    
+    answer = -1
+    mapmax = 0
+    for key in mapone:
+        if mapone[key] > mapmax:
+            answer =key
+            mapmax = mapone[key]
     '''Given an unsorted list of numbers, returns the value that occured the most in nums.'''
     # Hint: use oddOrEven to test function faster
     # Hint: use a map
     # Hint: https://stackoverflow.com/questions/13098638/how-to-iterate-over-the-elements-of-a-map-in-python
-    return -1 
+    return answer
 
 
 def main():
