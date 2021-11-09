622a16a757c241acabfe160528cbef4ef3067173
kvtravis <kvtravis@umich.edu>
2021-11-09 15:17:07 -0500

    final push for practice 3

--- main3.py
+++ main3.py
@@ -1,16 +1,25 @@
 #### INCLUDE ANY IMPORTS YOU NEED HERE, DO NOT PIP INSTALL ANY LIBRARIES ####
+from math import sqrt
+from random import randint
+import datetime
+from statistics import mean, mode, median
+import csv
 
 
 def perfect_square(num):
     '''Return the sqrt of <num> only if <num> is a perfect square, otherwise return -1.'''
     # Hint: math library
-    return 0
-
+    x = sqrt(num)
+    if x * x == num:
+        return int(x)
+    else:
+        return -1
+    
 
 def random_num_generator(min, max):
     '''Returns a random number between min and max inclusive.'''
     # Hint: random library
-    return 0
+    return randint(min, max)
 
 
 def get_today():
@@ -18,14 +27,39 @@ def get_today():
     # Note: Code must work regardless of today's date
     # Example: November 19, 2021
     # Hint: datetime library
-    return "November 19, 2021"
+
+    month = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+
+    today = datetime.datetime.now().strftime("%F")
+    todaysdate = today.split('-')
+    thedate = str(month[int(todaysdate[1])]) + " " + todaysdate[2] + ", " + todaysdate[0]
+
+    return thedate
 
 
 def get_stat(nums, type):
     '''Returns <type> of an unsorted list <nums>, where type can be mean, median, mode.'''
     # Example: get_stat([0, 1, 2], "median"), returns 1
     # Hint: statistics library
-    return 0
+    if type == "mean":
+        return mean(nums)
+    elif type == "median":
+        return median(nums)
+    else:
+        return mode(nums)
 
 
 def print_by_profit():
@@ -33,7 +67,15 @@ def print_by_profit():
     # Hint: csv library
     # Hint: use built-in sort() after parsing csv
     # Hint: figure out how to print out each row in csv first
-    print("Working with csvs!")
+    with open('data/sales_records.csv') as file:
+        rowdata = list(csv.reader(file, delimiter=","))
+    
+    rowdata.pop(0)
+
+    rowdata.sort(key=lambda i:float(i[12]))
+
+    for row in rowdata:
+        print (row)
 
 
 def main():
